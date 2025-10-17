becf1bed1b150312fbbf4f23fbfbf619a87d9410
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-10-17 12:26:26 -0300

    icono

--- pryDiesenberg-EjercicioPorResolverSP4/frmBarMilanga.Designer.cs
+++ pryDiesenberg-EjercicioPorResolverSP4/frmBarMilanga.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pryDiesenberg_EjercicioPorResolverSP4
 {
-    partial class Form1
+    partial class frmBarMilanga
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBarMilanga));
+            this.SuspendLayout();
+            // 
+            // frmBarMilanga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmBarMilanga";
+            this.Text = "Bar Milanga";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
